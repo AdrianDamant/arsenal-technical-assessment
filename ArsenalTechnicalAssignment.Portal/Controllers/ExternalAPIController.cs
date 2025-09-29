bfb18778e22abb7efd3820d373f7ec55c008cfaa
Adrian Mark Damant <132611861+AdrianDamant@users.noreply.github.com>
2025-09-29 15:59:47 +0100

    Project rebuild and publish

---
+++ ArsenalTechnicalAssignment.Portal/Controllers/ExternalAPIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArsenalTechnicalAssignment.Portal.Controllers
+{
+    public class ExternalAPIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
